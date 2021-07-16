f1f5ed60be7e9c9793eee1c98f68d02149dba29e
kkeeth <zensin0082@gmail.com>
2021-07-17 00:40:07 +0900

    feat: worker working

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
-  <div>Hello, MSW!!</div>
+  <div>
+    <ul>
+      <li :key="user.id" v-for="user in state.users">
+        <p>id: {{ user.id }}</p>
+        <p>name: {{ user.name }}</p>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, onMounted } from "vue";
+type User = {
+  id: number;
+  name: string;
+}
+
 export default defineComponent({
   name: "App",
   setup() {
-    if (process.env.NODE_ENV === 'development') {
-      import('./mocks/browser').then(module => {
-        module.worker.start()
-      })
-    }
+    const state = reactive({
+      users: [] as User[]
+    })
+
+    onMounted(async () => {
+      const res = await fetch('/users')
+      state.users = await res.json()
+    })
+
+    return { state }
   },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { worker } from './mocks/browser';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 createApp(App).mount('#app')
