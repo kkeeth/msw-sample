354f6112907a70d870b6b07a600b024a8e5909a9
kkeeth <zensin0082@gmail.com>
2021-07-16 18:45:04 +0900

    feat: connected worker and mock

--- src/App.vue
+++ src/App.vue
@@ -6,5 +6,12 @@
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "App",
+  setup() {
+    if (process.env.NODE_ENV === 'development') {
+      import('./mocks/browser').then(module => {
+        module.worker.start()
+      })
+    }
+  },
 });
 </script>
